bc4569c3e313df43f7ac44dba986b6ae70f4ab0c
Matheus Souza Silva <matheus_ss02@hotmail.com>
2024-07-31 08:42:13 -0300

    feat: added validate checkin service and tests

--- src/services/validate-check-in.ts
+++ src/services/validate-check-in.ts
@@ -5,6 +5,8 @@ import ResourceNotFoundError from './errors/resource-not-found-error'
 import { getDistanceBetweenTwoCoordinates } from './utils/get-distance-between-two-coordinates'
 import MaxNumberOfCheckInsError from './errors/max-number-of-check-ins-error'
 import MaxDistanceError from './errors/max-distance-error'
+import dayjs from 'dayjs'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 type ValidateCheckInServiceRequest = {
 	checkInId: string
@@ -24,6 +26,12 @@ export class ValidateCheckInService {
 
 		if (!checkIn) {
 			throw new ResourceNotFoundError()
+		}
+
+		const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(checkIn.created_at, 'minutes')
+
+		if (distanceInMinutesFromCheckInCreation > 20) {
+			throw new LateCheckInValidationError()
 		}
 
         checkIn.validated_at = new Date()
